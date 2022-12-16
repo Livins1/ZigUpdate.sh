044cc72e07fedb4ced7151d418f2214127992ff8
serruht <latemantis@gmail.com>
2022-12-16 23:14:59 +0800

    Last Edit?

---
+++ README.md
@@ -0,0 +1,22 @@
+### UpdateZig 
+a scirpt to update Ziglang from offical web, especially master branch.
+
+### Usage
+just put the updateZig.sh and the binary of zig together.
+```
+/zigPath
+├── /doc
+├── /lib
+├── LICENSE
+├── updateZig.sh
+├── zig
+└── zls
+```
+and cmd:
+```
+# bash 
+./updateZig.sh
+```
+Than, enjoy your time.
+
+
---
+++ updateZig.sh
@@ -0,0 +1,67 @@
+#/bin/bash
+
+
+TempPath="./tmp"
+arch="x86_64-linux"
+
+# Don't write space when set value in shell
+ZipUrl="NoUrlFound"
+Cversion=`zig version`
+echo "Current Zig Version: "$Cversion
+curl https://ziglang.org/download/index.json > index.json
+
+
+if [ ! -f ./index.json ]
+then
+    echo "Zig offcial Json File Download Filed, Please Retry."
+    exit
+else 
+    echo "Zig offcial Json File Download Successful, Start Parse VersionInfo."
+fi
+
+
+
+ZipUrl=`cat ./index.json | jq ' .master | ."'$arch'" | .tarball ' | tr -d '"'`
+echo $ZipUrl
+
+if [ ! ${#ZipUrl} -gt 5 ]
+then
+    echo "Url: "$ZipUrl 
+    echo " Parse Download Url Failed"
+    exit
+fi
+
+# GetFileName 
+FileName="NotFound"
+FileName=`echo "${ZipUrl##*/}"`
+NewVersion=`echo $FileName | grep -E '[0-9]\.[0-9]{2}\.[0-9][\^-]([a-z]+)\.([0-9a-z\+]+)' -o`
+echo "FindFileName: "$FileName" NewVersion: $NewVersion CurrentVersion: "$Cversion
+if [ $NewVersion = $Cversion ]
+then
+    echo "SameVersion with Zig offical server, ignore update"
+    exit
+fi
+
+# Downloadile
+echo "Start to Download New Version of Zig"
+if [ "$(curl -L -w '%{http_code}' $ZipUrl -o $FileName )" = "200" ]; 
+then
+    echo " Download Success"
+else
+    echo "Download Failed, Restart this scirpt may fix this problem."
+    exit
+fi
+
+
+# UnCompress
+mkdir -p $TempPath
+tar -xf $FileName -C $TempPath --strip-components 1
+
+echo "UnCompress Finished, Start to Delete and Update Zig to: $FileName "
+# Delele the old Version
+ls | grep -v 'zls\|tmp\|updateZig.sh' | xargs rm -rf
+# Retrive New Ziglang Prebuild Files
+cp -r ./tmp/* ./
+# Clean
+rm -rf ./tmp
+echo "Update Zig finished!"
